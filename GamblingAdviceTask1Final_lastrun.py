0df3b4d010045b25d7b04738fe5450558277512d
dfareri <dfareri@users.noreply.github.com>
2020-02-05 14:06:11 -0500

    data for 114

--- GamblingAdviceTask1Final_lastrun.py
+++ GamblingAdviceTask1Final_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on Wed Feb  5 11:50:10 2020
+    on Wed Feb  5 13:00:13 2020
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
