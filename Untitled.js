128eec3512401e9dfebeedb68e355ed36f0ed289
Dominic Fareri <dfareri@adelphi.edu>
2020-05-04 22:57:09 -0400

    added python code to fix data files

---
+++ Untitled.js
@@ -0,0 +1,7 @@
+endText.setColor(new util.Color('white'));
+    endText.setPos([(- 0.06), 0]);
+    endText.setText((((text2 + ' told you to gamble ') + gamAdvRisk) + ' times. \n \n')
+(('You followed their advice to gamble ' + advFollow) + ' times. \n \n')
+(('You gambled ' + nGam) + ' times. \n \n')
+(('You won the gambles ' + GamblePer) + '% of the time. \n \n')
+(('Please press enter to move on to the next block of trials. \n');
